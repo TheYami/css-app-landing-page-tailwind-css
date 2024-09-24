978bb7adcac6d2a6de076095d4ef4721531eaad2
Thanakon <thanakon.piti@gmail.com>
2024-09-24 15:37:47 +0700

    feat: add html input and footer style by tailwind on page

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').config} */
+module.exports = {
+  theme: {
+    color: {
+      gray: "#F6F6FB",
+      dark: "#1A1A2C",
+      lightgray: "#E0E0ED",
+      hover: "#B5A9FF",
+      focus: "#5E44FF",
+      hoverbuttom: "#5F5F76",
+      focusbuttom: "#35354D",
+      focusringbuttom: "#5F5F76",
+    },
+  },
+};
